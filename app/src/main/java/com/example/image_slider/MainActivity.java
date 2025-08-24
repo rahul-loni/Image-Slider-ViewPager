b7a0adff829bf796f42455d930dba25a8890f7de
Rahul Kumar <rahulismt491@gamail.com>
2025-08-25 00:43:40 +0545

    done

--- app/src/main/java/com/example/image_slider/MainActivity.java
+++ app/src/main/java/com/example/image_slider/MainActivity.java
@@ -2,19 +2,24 @@ package com.example.image_slider;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private ImagePagerAdapter adapter;
+    private Handler handler;
+    private Runnable runnable;
+    private int currentPage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,51 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        ImagePagerAdapter adapter = new ImagePagerAdapter();
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new ImagePagerAdapter();
         viewPager.setAdapter(adapter);
+
+        Button btnNext = findViewById(R.id.btnNext);
+        Button btnPrevious = findViewById(R.id.btnPrevious);
+
+        // Handle Next button
+        btnNext.setOnClickListener(v -> {
+            int next = (viewPager.getCurrentItem() + 1) % adapter.getCount();
+            viewPager.setCurrentItem(next, true);
+        });
+
+        // Handle Previous button
+        btnPrevious.setOnClickListener(v -> {
+            int prev = (viewPager.getCurrentItem() - 1 + adapter.getCount()) % adapter.getCount();
+            viewPager.setCurrentItem(prev, true);
+        });
+
+        // Auto slide logic
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage == adapter.getCount()) {
+                    currentPage = 0; // restart from first image
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+                handler.postDelayed(this, 3000); // 3 seconds delay
+            }
+        };
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, 3000); // start auto-slide
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable); // stop auto-slide when not visible
+    }
+
     private class ImagePagerAdapter extends PagerAdapter {
         private int[] mImages = new int[]{
                 R.drawable.processedmeat,
@@ -39,10 +85,12 @@ public class MainActivity extends AppCompatActivity {
         public int getCount() {
             return mImages.length;
         }
+
         @Override
         public boolean isViewFromObject(View view, Object object) {
             return view == ((ImageView) object);
         }
+
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             Context context = MainActivity.this;
@@ -55,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
             ((ViewPager) container).addView(imageView, 0);
             return imageView;
         }
+
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             ((ViewPager) container).removeView((ImageView) object);
